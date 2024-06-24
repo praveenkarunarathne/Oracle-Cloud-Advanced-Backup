9611581effb6a101471751522fc27c6f7c440136
Allinone24567 <112103617+Allinone24567@users.noreply.github.com>
2024-06-24 19:21:46 +0530

    Update AllInOne_Restoring_Backup.sh

--- AllInOne_Restoring_Backup.sh
+++ AllInOne_Restoring_Backup.sh
@@ -1,14 +1,23 @@
 clear
 echo -e "\n\t\t>>>>>            Starting Script            <<<<<\n"
 mkdir AllInOne_Folder
-tar -xzf $(ls | grep AllInOne_Backup_) --preserve-permissions --same-owner --ignore-failed-read --overwrite
-mkfs.vfat -F 32 -i $(sed -n '1p' FS_Data.txt) /dev/$(lsblk -lf | grep sda | grep vfat | awk '{print $1}') 2>/dev/null
-mount -t vfat /dev/$(lsblk -lf | grep sda | grep vfat | awk '{print $1}') AllInOne_Folder
-tar -xzf vfat.tar.gz --preserve-permissions --same-owner --ignore-failed-read --overwrite
-umount AllInOne_Folder
 mount -t ext4 /dev/$(lsblk -lf | grep sda | grep ext4 | awk '{print $1}') AllInOne_Folder
-rm -r AllInOne_Folder/*
-tar -xzf ext4.tar.gz --preserve-permissions --same-owner --ignore-failed-read --overwrite
+if ls AllInOne_Backup_* 1> /dev/null 2>&1; then
+  rm -r AllInOne_Folder/*
+  tar -xzf $(ls | grep AllInOne_Backup_) --preserve-permissions --same-owner --ignore-failed-read --overwrite -C AllInOne_Folder
+  rm $(ls | grep AllInOne_Backup_)
+else
+  cd AllInOne_Folder
+  mv $(find . -name "AllInOne_Backup_*" -print -quit) .
+  find . -not -name "AllInOne_Backup_*" -delete
+  tar -xzf $(ls | grep AllInOne_Backup_) --preserve-permissions --same-owner --ignore-failed-read --overwrite
+  rm $(ls | grep AllInOne_Backup_)
+  cd
+fi
+mv AllInOne_Folder/vfat.tar.gz .
+mv AllInOne_Folder/FS_Data.txt .
+tar -xzf AllInOne_Folder/ext4.tar.gz --preserve-permissions --same-owner --ignore-failed-read --overwrite
+rm AllInOne_Folder/ext4.tar.gz
 rm -r AllInOne_Folder/root/AllInOne_Folder
 for config_file in AllInOne_Folder/etc/cloud/cloud.cfg.d/*.cfg; do
     if grep -q "network:" "$config_file"; then
@@ -22,6 +31,10 @@ else
     cp /etc/cloud/cloud.cfg.d/* AllInOne_Folder/etc/cloud/cloud.cfg.d/
 fi
 umount AllInOne_Folder
+mkfs.vfat -F 32 -i $(sed -n '1p' FS_Data.txt) /dev/$(lsblk -lf | grep sda | grep vfat | awk '{print $1}') 2>/dev/null
+mount -t vfat /dev/$(lsblk -lf | grep sda | grep vfat | awk '{print $1}') AllInOne_Folder
+tar -xzf vfat.tar.gz --preserve-permissions --same-owner --ignore-failed-read --overwrite
+umount AllInOne_Folder
 e2fsck -p -f /dev/$(lsblk -lf | grep sda | grep ext4 | awk '{print $1}')
 tune2fs -U $(sed -n '2p' FS_Data.txt) /dev/$(lsblk -lf | grep sda | grep ext4 | awk '{print $1}')
 fatlabel /dev/$(lsblk -lf | grep sda | grep vfat | awk '{print $1}') $(sed -n '3p' FS_Data.txt)
@@ -31,10 +44,8 @@ if [ -z "$LABEL" ]; then
 else
   e2label /dev/$(lsblk -lf | grep sda | grep ext4 | awk '{print $1}') $LABEL
 fi
-rm ext4.tar.gz
 rm vfat.tar.gz
 rm FS_Data.txt
-rm $(ls | grep AllInOne_Backup_)
 rm -r AllInOne_Folder
 rm AllInOne_Restoring_Backup.sh
 echo -e "\t\t>>>>>      Restoring Backup Successful      <<<<<"
